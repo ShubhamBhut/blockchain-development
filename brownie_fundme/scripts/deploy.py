da8b1e2bf63eef8d29d825660c3ae3a37ba660e8
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-15 13:15:04 +0530

    Brownie FundMe
    Funding and withdrawing in a contract using brownie. Also set up to test in local ganache via mocks, goerli testnet and mainnet fork by alchemy

---
+++ brownie_fundme/scripts/deploy.py
@@ -0,0 +1,31 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+from web3 import Web3
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the pricefeed address to fundme contract
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
---
+++ brownie_fundme/scripts/fund_and_withdraw.py
@@ -0,0 +1,24 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    # entrance_fee = 25 * 10**18
+    print(entrance_fee)
+    print(f"The current entrance fee is {entrance_fee}")
+    print("Funding")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account, "allow_revert": True})
+
+
+def main():
+    fund()
+    withdraw()
