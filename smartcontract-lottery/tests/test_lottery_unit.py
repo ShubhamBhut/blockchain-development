6e23220d8802a3f5156b390f972c6777e95c570a
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-23 13:45:22 +0530

    Add conditional-entrance testing

--- smartcontract-lottery/tests/test_lottery_unit.py
+++ smartcontract-lottery/tests/test_lottery_unit.py
@@ -1,9 +1,12 @@
-from brownie import Lottery, accounts, config, network
+from brownie import Lottery, accounts, config, network, exceptions
 from web3 import Web3
 from scripts.deploy_lottery import deploy_lottery
-
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+import pytest
 
 def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
     #Arrange
     lottery = deploy_lottery()
     #Act
@@ -12,4 +15,13 @@ def test_get_entrance_fee():
     entrance_fee = lottery.getEntranceFee()
     #Assert
     assert expected_entrance_fee == entrance_fee
+
+def test_cant_enter_unless_started():
+    #Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    #Act/ Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
 
