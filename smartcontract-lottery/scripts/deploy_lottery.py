2dc5f814500bcda6d1654ab22a739dd0e1b4704b
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-23 13:34:19 +0530

    Add local ganache unit test

--- smartcontract-lottery/scripts/deploy_lottery.py
+++ smartcontract-lottery/scripts/deploy_lottery.py
@@ -5,7 +5,7 @@ import time
 
 def deploy_lottery():
     account = get_account()
-    Lottery.deploy(
+    lottery = Lottery.deploy(
             get_contract("eth_usd_price_feed").address,
             get_contract("vrf_coordinator").address,
             get_contract("link_token").address,
@@ -15,6 +15,7 @@ def deploy_lottery():
             publish_source = config["networks"][network.show_active()].get("verify", False)
     )
     print("Deployed lottery!")
+    return lottery
 
 def start_lottery():
     account = get_account()
--- smartcontract-lottery/tests/test_lottery_unit.py
+++ smartcontract-lottery/tests/test_lottery_unit.py
@@ -1,13 +1,15 @@
 from brownie import Lottery, accounts, config, network
 from web3 import Web3
+from scripts.deploy_lottery import deploy_lottery
 
 
 def test_get_entrance_fee():
-    account = accounts[0]
-    lottery = Lottery.deploy(
-        config["networks"][network.show_active()]["eth_usd_price_feed"],
-        {"from": account}
-    )
-    assert lottery.getEntranceFee() > Web3.toWei(0.0006, "ether")
-    assert lottery.getEntranceFee() > Web3.toWei(0.018, "ether")
+    #Arrange
+    lottery = deploy_lottery()
+    #Act
+    #according to our mocks, 2000ETH = 1usd hence 2000/1 == 50/x and so x==0.025
+    expected_entrance_fee = Web3.toWei(0.025, "ether")
+    entrance_fee = lottery.getEntranceFee()
+    #Assert
+    assert expected_entrance_fee == entrance_fee
 
