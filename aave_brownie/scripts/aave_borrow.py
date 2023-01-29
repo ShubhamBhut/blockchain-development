f350a5a231ae6e7d59df19fd871e46a261a774ed
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-29 16:53:22 +0530

    implement Borrowing feature

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -23,7 +23,7 @@ def approve_erc20(amount, spender, erc20_address, account):
     return tx
 
 
-amount = 0.1 * 10**18
+amount = Web3.toWei(0.1, "ether")
 
 
 def get_borrowable_data(lending_pool, account):
@@ -36,17 +36,20 @@ def get_borrowable_data(lending_pool, account):
         health_factor,
     ) = lending_pool.getUserAccountData(account.address)
     available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
-    total_collateral_eth= Web3.fromWei(total_collateral_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
     total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
     print(f"You have {total_collateral_eth} worth of ETH deposited")
     print(f"You have {total_debt_eth} worth of ETH borrowed")
     print(f"You can borrow {available_borrow_eth} worth of ETH.")
     return (float(available_borrow_eth), float(total_debt_eth))
 
+
 def get_asset_price(price_feed_address):
-    pass
-
-
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f"The DAI/ETH price is {converted_latest_price}")
+    return float(converted_latest_price)
 
 
 def main():
@@ -71,12 +74,35 @@ def main():
     print("Deposited!")
 
     # Borrowning process
-    borrowable_eth, total_debt =  get_borrowable_data(lending_pool, account)
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
     print("Let's borrow!")
-    #DAI in terms of ether
-    if network.show_active() == 'goerli':
-        dai_usd_price = get_asset_price(config['networks'][network.show_active()]['dai_eth_price_feed'])
-        eth_usd_price = get_asset_price(config['networks'][network.show_active()]['dai_eth_price_feed'])
-        dai_eth_price =  dai_usd_price / eth_usd_price
+    # DAI in terms of ether
+    if network.show_active() == "goerli":
+        dai_usd_price = get_asset_price(
+            config["networks"][network.show_active()]["dai_eth_price_feed"]
+        )
+        eth_usd_price = get_asset_price(
+            config["networks"][network.show_active()]["dai_eth_price_feed"]
+        )
+        dai_eth_price = dai_usd_price / eth_usd_price
     else:
-        dai_eth_price = get_asset_price(config['networks'][network.show_active()]['dai_eth_price_feed'])
+        dai_eth_price = get_asset_price(
+            config["networks"][network.show_active()]["dai_eth_price_feed"]
+        )
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    # borrowable_eth -> borrowable_dai * 95%
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI")
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,
+        0,
+        account.address,
+        {"from": account},
+    )
+    borrow_tx.wait(1)
+    print("We borrowed some DAI")
+    get_borrowable_data(lending_pool, account)
+
+
