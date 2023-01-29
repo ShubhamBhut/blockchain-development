2373dd6f90ec225cf4bb9665b3e04e12186282c6
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-29 09:39:48 +0530

    Add IWethDeposit interface

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -3,9 +3,16 @@ from brownie import network, config
 from scripts.get_weth import get_weth
 
 
+def get_lending_pool():
+    
+
 def main():
     account = get_account()
     erc20_address = config['networks'][network.show_active()]['weth_token']
-    #get_weth()
     if network.show_active() in ['mainnet-fork']:
         get_weth()
+    #ABI
+    #Address
+    lending_pool = get_lending_pool()
+
+
--- aave_brownie/scripts/get_weth.py
+++ aave_brownie/scripts/get_weth.py
@@ -2,8 +2,6 @@ from brownie import interface, network
 from scripts.helpful_scripts import get_account, config
 
 def get_weth():
-    #ABI
-    #Address
     account = get_account()
     weth = interface.WethInterface(config["networks"][network.show_active()]["weth_token"])
     tx = weth.deposit({"from": account, "value": 0.1* 10**18})
