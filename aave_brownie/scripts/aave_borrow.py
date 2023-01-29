f5820fd2174b6a2b42c40d0c184d02143194ac2d
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-29 12:51:20 +0530

    Implement get borrowable_data

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -1,42 +1,73 @@
 from scripts.helpful_scripts import get_account
+from web3 import Web3
 from brownie import network, config, interface
 from scripts.get_weth import get_weth
 
 
 def get_lending_pool():
     lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
-            config['networks'][network.show_active()]['lending_pool_addresses_provider']
-            )
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
     lending_pool_address = lending_pool_addresses_provider.getLendingPool()
     lending_pool = interface.ILendingPool(lending_pool_address)
     return lending_pool
-    
+
+
 def approve_erc20(amount, spender, erc20_address, account):
     print("Approving ERC20 token.....")
-    #ABI
+    # ABI
     erc20 = interface.IERC20(erc20_address)
     tx = erc20.approve(spender, amount, {"from": account})
     tx.wait(1)
     print("Approved!")
     return tx
-    
+
+
 amount = 0.1 * 10**18
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth= Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited")
+    print(f"You have {total_debt_eth} worth of ETH borrowed")
+    print(f"You can borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
+
+
+
+
 
 def main():
     account = get_account()
-    erc20_address = config['networks'][network.show_active()]['weth_token']
-    if network.show_active() in ['mainnet-fork']:
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
         get_weth()
-    #ABI
-    #Address
+    # ABI
+    # Address
     lending_pool = get_lending_pool()
     print(lending_pool)
-    
-    #Approve of sending out ERC20 tokens
+
+    # Approve of sending out ERC20 tokens
     approve_erc20(amount, lending_pool.address, erc20_address, account)
+
+    # Depositing process
     print("Depositing...")
-    tx = lending_pool.deposit(erc20_address, amount, account.address, 0, {"from": account})
+    tx = lending_pool.deposit(
+        erc20_address, amount, account.address, 0, {"from": account}
+    )
     tx.wait(1)
     print("Deposited!")
 
-
+    # Borrowning process
+    borrowable_eth, total_debt =  get_borrowable_data(lending_pool, account)
--- aave_brownie/scripts/get_weth.py
+++ aave_brownie/scripts/get_weth.py
@@ -1,13 +1,17 @@
 from brownie import interface, network
 from scripts.helpful_scripts import get_account, config
 
+
 def get_weth():
     account = get_account()
-    weth = interface.WethInterface(config["networks"][network.show_active()]["weth_token"])
-    tx = weth.deposit({"from": account, "value": 0.1* 10**18})
+    weth = interface.WethInterface(
+        config["networks"][network.show_active()]["weth_token"]
+    )
+    tx = weth.deposit({"from": account, "value": 0.1 * 10**18})
     tx.wait(1)
-    print(f"Received 0.1 wETH")
+    print(f"Received 0.1 WETH")
     return tx
+
 
 def main():
     get_weth()
