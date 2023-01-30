230cbd723ed17bf586fc36488351c566f99c801f
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-30 16:34:22 +0530

    Add test for brownie ERC721

---
+++ brownie_nft/tests/unit/test_simple_collectible.py
@@ -0,0 +1,13 @@
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+import pytest
+from brownie import network
+from scripts.deploy_and_create import deploy_and_create
+from scripts.helpful_scripts import get_account
+
+def test_can_create_simple_collectible():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    simple_collectible = deploy_and_create()
+    assert simple_collectible.ownerOf(0) == get_account()
+    
+    
