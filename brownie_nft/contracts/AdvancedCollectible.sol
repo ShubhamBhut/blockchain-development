8a92e495a12c5bc622ef60467e808b1421dc2bb1
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-30 20:12:59 +0530

    Add Mocks for AdvanceNFT

--- brownie_nft/contracts/AdvancedCollectible.sol
+++ brownie_nft/contracts/AdvancedCollectible.sol
@@ -5,7 +5,7 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract AdvancedCollectible is ERC721{
+contract AdvancedCollectible is ERC721, VRFConsumerBase{
 	
 	uint256 public tokenCounter;
 	bytes32 public keyHash;
