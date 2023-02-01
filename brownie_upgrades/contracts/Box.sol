0f7d5ea233343ee3accd2b073c7de0a2097a72a0
Shubham Patel <shubhamprecious1@gmail.com>
2023-02-01 16:58:56 +0530

    add deploy_box

--- brownie_upgrades/contracts/Box.sol
+++ brownie_upgrades/contracts/Box.sol
@@ -1,20 +1,21 @@
-//SPDX-License-Identifier: MIT
-
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+ 
 contract Box {
-	
-	uint256 private value;
-
-	event valueChanged(uint256 newValue);
-
-	function store(uint256 newValue) public {
-		value = newValue;
-		emit valueChanged(newValue);
-	}
-
-	function retrieve() public returns (uint256) {
-		return value;
-		
-	}
+    uint256 private value;
+ 
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newValue);
+ 
+    // Stores a new value in the contract
+    function store(uint256 newValue) public {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+ 
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
 }
--- brownie_upgrades/scripts/01_deploy_box.py
+++ brownie_upgrades/scripts/01_deploy_box.py
@@ -1,5 +1,10 @@
 from scripts.helpful_scripts import get_account
+from brownie import network, Box
+
 
 def main():
     account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    box = Box.deploy({"from": account})
+    print("this is retrieve", box.retrieve())
 
